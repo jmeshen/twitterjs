b28c5062eced8d458c8d978c746428530ba53025
Jamie <jmeshen@gmail.com>
2015-04-29 19:14:34 -0400

    finished up to sockets

--- app.js
+++ app.js
@@ -3,9 +3,22 @@ var app = express();
 var morgan = require('morgan');
 app.use(morgan('dev'));
 var swig = require('swig');
+var socketio = require('socket.io');
+var bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+var server = app.listen(3000);
+var io = socketio.listen(server);
 var routes = require('./routes/');
-app.use('/', routes);
+// app.use('/', routes);
+app.use( '/', routes(io) );
 app.use(express.static(__dirname + '/public'));
+
+
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 app.engine('html', swig.renderFile);
@@ -20,9 +33,11 @@ swig.setDefaults({ cache: false });
 //   res.render( 'index', {title: 'Hall of Fame', people: people} );
 // });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+// var server = app.listen(3000, function () {
+//   var host = server.address().address;
+//   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+//   console.log('Example app listening at http://%s:%s', host, port);
+// });
+
+
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,36 @@ var router = express.Router();
 // could use one line instead: var router = require('express').Router();
 var tweetBank = require('../tweetBank');
 
-router.get('/', function (req, res) {
-  var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
-});
 
-module.exports = router;
+module.exports = function(io) {
+  router.post('/submit', function(req, res) {
+    var name = req.body.name;
+    var text = req.body.text;
+    var tweetArr = tweetBank.add(name, text);
+    console.log(tweetArr[tweetArr.length - 1].tweetAndId.text);
+    console.log('this is io: ' + io);
+    io.sockets.emit('new_tweet', { tweet: tweetArr[tweetArr.length - 1].tweetAndId.text });
+    res.redirect('/');
+  });
+
+  router.get('/', function (req, res) {
+    var tweets = tweetBank.list();
+    res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true } );
+  });
+
+  router.get('/users/:name', function(req, res) {
+    var name = req.params.name;
+    var list = tweetBank.find( {name: name} );
+    res.render( 'index', { title: 'Twitter.js - Posts by ' + name, tweets: list, showForm: true, name: name } );
+
+  });
+
+  router.get('/users/:name/tweets/:id', function(req, res) {
+    var name = req.params.name;
+    var id = req.params.id;
+    var list = tweetBank.find( {id: id} );
+    res.render( 'index', { title: 'Twitter.js - Posts by ' + name, tweets: list } );
+  });
+  return router;
+// module.exports = router;
+}
