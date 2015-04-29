6a917c5209194a8a4716908f1337299a04fee8a4
Jamie <jmeshen@gmail.com>
2015-04-29 16:05:53 -0400

    working front page route

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 var express = require('express');
-var morgan = require('morgan');
 var app = express();
-
+var morgan = require('morgan');
 app.use(morgan('dev'));
+var swig = require('swig');
+var routes = require('./routes/');
+app.use('/', routes);
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.engine('html', swig.renderFile);
+swig.setDefaults({ cache: false });
+
+// var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+// res.render( 'index', {title: 'Hall of Fame', people: people} );
+
+// app.get('/', function (req, res) {
+//   // res.send('Hello World!');
+//   var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+//   res.render( 'index', {title: 'Hall of Fame', people: people} );
+// });
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-// hello
